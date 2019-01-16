c0d2c057476eacf823db7a4b935abdc8638cb481
Cristian Felipe <cristianpipe0494@gmail.com>
2019-01-16 11:13:37 -0600

    Have added functionality to the event's btn, arrow and slider.

--- js/script.js
+++ js/script.js
@@ -6,13 +6,30 @@ const bucketListDom = document.getElementById('js-bucket-event-list');
 const mainEventDom = document.getElementById('js-main-event');
 const mainEventBtn = document.getElementById('js-main-event-btn');
 const mainEventImgWrapper = document.getElementById('js-img-wrapper');
+const leftArrowDom = document.getElementById('js-left-arrow');
+const rightArrowDom = document.getElementById('js-right-arrow');
 
 // Add event listener.
 // Function will run as page Loads.
 xmlhttp.addEventListener ('load', function() {
 
+  // Initilialized elements internally in main function.
+  // This variables will be use on several functions.
+  let mainEventDate;
+  let mainEventName;
+  let mainEventLocation;
+  let mainEventURL;
+  let mainEventImg;
+  let current;
+
+  // Array.
+  const eventsArray = [];
+
   // Save the response elements.
   const response = xmlhttp.response.events;
+
+  // Iterate response elements.
+  // Create DOM elements for each JSON's element.
   for (let e = 0; e < response.length; e++) {
 
     const eventHardCodedDate = response[e].hardcoded;
@@ -42,27 +59,34 @@ xmlhttp.addEventListener ('load', function() {
     listItemEvent.appendChild(listItemEventLocation);
     listItemEvent.appendChild(listItemEventURL);
 
+    // Create an array of elements all ready created.
+    // Push elements into array to create iterations and change element's styles and info.
+    eventsArray.push(listItemEvent);
+
+    // Add element with completed information into DOM element.
     bucketListDom.appendChild(listItemEvent);
 
+    // If the element is 0 Index.
+    // Element and information will be set in the main Event Banner.
     if (e === 0) {
 
       listItemEvent.setAttribute('class', 'activeStateEvent');
 
-      const mainEventDate = document.createElement('h2');
+      mainEventDate = document.createElement('h2');
       mainEventDate.innerHTML = eventHardCodedDate;
 
-      const mainEventName = document.createElement('h3');
+      mainEventName = document.createElement('h3');
       mainEventName.innerHTML = eventName;
 
-      const mainEventLocation = document.createElement('h4');
+      mainEventLocation = document.createElement('h4');
       mainEventLocation.innerHTML = eventLocation;
 
-      const mainEventURL = document.createElement('a');
+      mainEventURL = document.createElement('a');
       mainEventURL.setAttribute('href', eventTicketsURL);
       mainEventURL.setAttribute('target', 'bank');
       mainEventURL.innerHTML = 'Get Events Details';
 
-      const mainEventImg = document.createElement('img');
+      mainEventImg = document.createElement('img');
       mainEventImg.setAttribute('class', 'event-backgroung-img');
       mainEventImg.setAttribute('src', 'img/bg-event-1-optimized.jpg')
       mainEventImg.setAttribute('alt', 'main event photo');
@@ -73,7 +97,118 @@ xmlhttp.addEventListener ('load', function() {
       mainEventBtn.appendChild(mainEventURL);
       mainEventImgWrapper.appendChild(mainEventImg);
     }
+
+    // EventListener will clear array elements's style.
+    // And set new information on main event banner.
+    listItemEvent.addEventListener('click', function() {
+
+      // This function will clear styles from array's elements.
+      // And will set new styles for the one clicked.
+      eventsArray.forEach(element => {
+        element.setAttribute('class', 'deactivateStateEvent');
+      });
+      listItemEvent.setAttribute('class', 'activeStateEvent');
+
+      mainEventDate.innerHTML = '';
+      mainEventName.innerHTML = '';
+      mainEventLocation.innerHTML = '';
+
+      mainEventDate.innerHTML = listItemEventDate.innerText;
+      mainEventName.innerHTML = listItemEventName.innerText;
+      mainEventLocation.innerHTML = listItemEventLocation.innerText;
+      mainEventURL.setAttribute('href', listItemEventURL);
+
+      // Every click function will get the index of the clickable event.
+      // And will change photo.
+      let clickIndex = eventsArray.indexOf(listItemEvent);
+      let imgArrayIndex = bgImgArray[clickIndex];
+      mainEventImg.setAttribute('src', imgArrayIndex);
+
+      // Current element clicked will change to generate.
+      // A different order in slide show.
+      current = clickIndex;
+    });
   };
+
+
+  const bgImgArray = [
+    '../img/bg-event-1-optimized.jpg',
+    '../img/bg-event-2-optimized.jpg',
+    '../img/bg-event-3-optimized.jpg',
+    '../img/bg-event-4-optimized.jpg',
+    '../img/bg-event-5-optimized.jpg'
+  ];
+
+  slider();
+  function slider() {
+
+    // Initialized index to 0.
+    current = 0;
+
+    //Show preview.
+    function slideLeft (){
+      let imgArrayIndex = bgImgArray[current - 1];
+      mainEventImg.setAttribute('src', imgArrayIndex);
+      eventsArray.forEach(element => {
+        element.setAttribute('class', 'deactivateStateEvent');
+      });
+      let clickIndex = eventsArray[current - 1];
+      clickIndex.setAttribute('class', 'activeStateEvent');
+
+      mainEventDate.innerHTML = '';
+      mainEventName.innerHTML = '';
+      mainEventLocation.innerHTML = '';
+
+      mainEventDate.innerHTML = eventsArray[current - 1].children["0"].firstChild.data;
+      mainEventName.innerHTML = eventsArray[current - 1].children["1"].firstChild.data;
+      mainEventLocation.innerHTML = eventsArray[current - 1].children["2"].firstChild.data;
+      let listItemEventURL = eventsArray[current - 1].children["3"].href;
+      mainEventURL.setAttribute('href', listItemEventURL);
+
+      current--;
+    }
+
+    //Show preview.
+    function slideRight (){
+      let imgArrayIndex = bgImgArray[current + 1];
+      mainEventImg.setAttribute('src', imgArrayIndex);
+      eventsArray.forEach(element => {
+        element.setAttribute('class', 'deactivateStateEvent');
+      });
+      let clickIndex = eventsArray[current + 1];
+      clickIndex.setAttribute('class', 'activeStateEvent');
+
+      mainEventDate.innerHTML = '';
+      mainEventName.innerHTML = '';
+      mainEventLocation.innerHTML = '';
+
+      mainEventDate.innerHTML = eventsArray[current + 1].children["0"].firstChild.data;
+      mainEventName.innerHTML = eventsArray[current + 1].children["1"].firstChild.data;
+      mainEventLocation.innerHTML = eventsArray[current + 1].children["2"].firstChild.data;
+      let listItemEventURL = eventsArray[current + 1].children["3"].href;
+      mainEventURL.setAttribute('href', listItemEventURL);
+
+      current++;
+    }
+
+    // Left Arrow click.
+    // Function will run on click.
+    leftArrowDom.addEventListener('click', function (){
+      if (current === 0){
+          current = bgImgArray.length;
+      }
+      slideLeft();
+    });
+
+    // Right Arrow click.
+    // Function will run on click.
+    rightArrowDom.addEventListener('click', function (){
+      if (current === bgImgArray.length-1){
+          current =- 1;
+      }
+      slideRight();
+    });
+  }
 });
 
 // Response the system is waiting.
